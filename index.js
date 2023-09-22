2366e4b05a6a293c668ab31573dd0badfc306a70
Carla Yareny Gómez Bolaños <isc20350272@gmail.com>
2023-09-22 13:44:43 -0600

    cuarto commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+//http://localhost:3000/carla/123
+app.get('/:user/:password', (req, res) =>{
+    const {user, password} = req.params
+    
+    if (user === 'carla' && password === '123'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+    res.status(404).json({msg: 'Error en el usuario o la contraseña'})
+})
+//http://localhost:3000/login?user=carla&password=123
+app.get('/login', (req, res) =>{
+    const {user, password} = req.query
+if(!user || !password){
+    res.status(400).json({
+        msg: 'You need to provide <user> and <password> paramenters'
+    })
+    return
+}
+
+if (user === 'carla' && password === '123'){
+    res.json({msg: 'Inicio de sesion exitoso'})
+    return
+}
+res.status(404).json({msg: 'Error en el usuario o la contraseña'})
+
+})
+
+/*FUNCIONALIDAD*/
+
+app.post('/', (req, res) => {res.json({msg: 'Hola POST'})})
+
+app.put('/', (req, res) =>{res.json({msg:'Hola PUT'})})
+
+app.patch('/', (req, res) =>{res.json({msg:'Hola PATCH'})})
+
+app.delete('/', (req, res) =>{res.json({msg:'Hola DELETE'})})
+
+//http://localhost:3000/
+app.listen(3000, () => {
+    console.log('listenig on port 3000')
+})
